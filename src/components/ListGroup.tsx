03d07f545e2e5dbf266ce55b52cf2122e199b1ab
Michael Pack <packmichael@hotmail.com>
2023-08-24 20:26:30 -0230

    conditional rendering added

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,9 +1,11 @@
 function ListGroup() {
-  const items = ["New York", "San Fransico", "Toyko", "London", "Paris"];
+  let items = ["New York", "San Fransico", "Toyko", "London", "Paris"];
+  items = [];
 
   return (
     <>
       <h1>List</h1>
+      {items.length === 0 && <p>No item found</p>}
       {items.map((item) => (
         <li key={item}>{item}</li>
       ))}

78e4b027a3d4358e93faa0716482d8e79a3bab99
Michael Pack <packmichael@hotmail.com>
2023-08-26 15:52:16 -0230

    handling events changing states

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,15 +1,29 @@
+import { useState } from "react";
+
 function ListGroup() {
   let items = ["New York", "San Fransico", "Toyko", "London", "Paris"];
-
+  const [selectedIndex, setSelectedIndex] = useState(-1);
   return (
     <>
       <h1>List</h1>
       {items.length === 0 && <p>No item found</p>}
-      {items.map((item) => (
-        <li className="list-group-item" key={item}>
-          {item}
-        </li>
-      ))}
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
     </>
   );
 }

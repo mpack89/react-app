24c4aa06989e024eb3bf74664139d0702081d793
Michael Pack <packmichael@hotmail.com>
2023-08-26 16:51:12 -0230

    list group additions

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import ListGroup from "./components/ListGroup";
 
 function App() {
+  let items = ["New York", "San Fransico", "Toyko", "London", "Paris"];
   return (
     <div>
-      <ListGroup />
+      <ListGroup items={items} heading="Cities" />
     </div>
   );
 }

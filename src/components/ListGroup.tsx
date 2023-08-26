1f6a9b236232a93558b603b14ff108d10ebe9e3f
Michael Pack <packmichael@hotmail.com>
2023-08-25 22:50:11 -0230

    style edits- bootsrap added

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,13 +1,14 @@
 function ListGroup() {
   let items = ["New York", "San Fransico", "Toyko", "London", "Paris"];
-  items = [];
 
   return (
     <>
       <h1>List</h1>
       {items.length === 0 && <p>No item found</p>}
       {items.map((item) => (
-        <li key={item}>{item}</li>
+        <li className="list-group-item" key={item}>
+          {item}
+        </li>
       ))}
     </>
   );

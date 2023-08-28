7c2d03c016edbe0308d92a925d99c7e76d4dbc4c
Michael Pack <packmichael@hotmail.com>
2023-08-28 20:46:51 -0230

    like button created

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import { useState } from "react";
 import Alert from "./components/Alert";
 import Button from "./components/Button";
+import Like from "./components/Like";
 
 function App() {
-  const [alertVisible, setAlertVisibility] = useState(false);
   return (
     <div>
-      {alertVisible && (
-        <Alert onClose={() => setAlertVisibility(false)}>My alert</Alert>
-      )}
-      <Button onClick={() => setAlertVisibility(true)}>My Button</Button>
+      <Like onClick={() => console.log("clicked")} />
     </div>
   );
 }
